fba9edb4ed07f98eb143953e5452b53c658850f1
here-bossnanda <m.trinandanoviardy@gmail.com>
2021-04-07 02:26:09 +0700

    [nanda] add auth middleware

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"crowfunding/auth"
 	"crowfunding/handler"
+	"crowfunding/helper"
 	"crowfunding/user"
 	"log"
+	"net/http"
+	"strings"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -31,8 +35,53 @@ func main() {
 	api.POST("/register", userHandler.RegisterUser)
 	api.POST("/login", userHandler.Login)
 	api.POST("/email_checker", userHandler.CheckEmailAvailability)
-	api.POST("/avatar", userHandler.UploadAvatar)
+	api.POST("/avatar",authMiddleware(authService, userService), userHandler.UploadAvatar)
 
 
 	router.Run(":8090")
 }
+
+func authMiddleware(authService auth.Service, userService user.Service) gin.HandlerFunc {
+	return func (c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if !strings.Contains(authHeader, "Bearer") {
+			response := helper.APIResponse("Unauthorized", http.StatusUnauthorized, "error", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized,response)
+			return
+		}
+	
+		var stringToken string
+	
+		splitToken := strings.Split(authHeader, " ")
+		if len(splitToken) == 2 {
+			stringToken = splitToken[1]
+		}
+	
+		token, err := authService.ValidateToken(stringToken)
+		if err != nil {
+			response := helper.APIResponse("Unauthorized", http.StatusUnauthorized, "error", err.Error())
+			c.AbortWithStatusJSON(http.StatusUnauthorized,response)
+			return
+		}
+
+		payload, ok := token.Claims.(jwt.MapClaims)
+
+		if !ok || !token.Valid {
+			response := helper.APIResponse("Unauthorized", http.StatusUnauthorized, "error", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized,response)
+			return
+		}
+
+		userId := int(payload["userId"].(float64))
+
+		user, err := userService.GetUserById(userId)
+		if err != nil {
+			response := helper.APIResponse("Unauthorized", http.StatusUnauthorized, "error", err.Error())
+			c.AbortWithStatusJSON(http.StatusUnauthorized,response)
+			return
+		}
+
+		c.Set("currentUser", user)
+	}
+}
+
