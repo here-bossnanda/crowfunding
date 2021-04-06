e3880538f18ae994a037d48ccd16fe49d61916d6
here-bossnanda <m.trinandanoviardy@gmail.com>
2021-04-07 02:59:12 +0700

    [nanda] add initial campaign

---
+++ campaign/entity.go
@@ -0,0 +1,26 @@
+package campaign
+
+import "time"
+
+type Campaign struct {
+	ID  						 int
+	userId 					 int
+	ShortDescription string
+	Description 		 string
+	Perks 					 string
+	BackerCount 		 int
+	GoalAmount 			 int
+	CurrentAmount 	 int
+	Slug 						 string
+	CreatedAt 			 time.Time
+	UpdatedAt 			 time.Time
+}
+
+type CampaignImage struct {
+	ID 					int
+	CampaignId  int
+	FileName 		string
+	IsThumbnail int
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
