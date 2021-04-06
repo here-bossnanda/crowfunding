34632cf68e7d9f52fa3e82372a995b4e9d776ee9
here-bossnanda <m.trinandanoviardy@gmail.com>
2021-04-06 17:25:32 +0700

    [nanda] test http request gin-gonic

--- main.go
+++ main.go
@@ -2,15 +2,38 @@ package main
 
 import (
 	"crowfunding/user"
-	"fmt"
 	"log"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
-	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+	// dsn := "root:@tcp(127.0.0.1:3306)/dbcrowfunding?charset=utf8mb4&parseTime=True&loc=Local"
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	// if err != nil{ 
+	// 	log.Fatal(err.Error())
+	// }
+
+	// fmt.Println("Database is connected!")
+
+	// var users []user.User
+	// db.Find(&users)
+
+	// for _, user := range users {
+	// 	fmt.Println(user.Name)
+	// 	fmt.Println(user.Email)
+	// }
+
+	router := gin.Default()
+	router.GET("/users", handler)
+	router.Run(":8090")
+}
+
+func handler(c *gin.Context) {
 	dsn := "root:@tcp(127.0.0.1:3306)/dbcrowfunding?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
@@ -18,20 +41,8 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
-	fmt.Println("Database is connected!")
-
 	var users []user.User
-	length := len(users)
-
-	fmt.Println(length)
-
 	db.Find(&users)
 
-	length = len(users)
-	fmt.Println(length)
-
-	for _, user := range users {
-		fmt.Println(user.Name)
-		fmt.Println(user.Email)
-	}
+	c.JSON(http.StatusOK, users)
 }
