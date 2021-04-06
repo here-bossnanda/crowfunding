91f995df1826ce3a4f6f7d6a233abb1ebbd95211
here-bossnanda <m.trinandanoviardy@gmail.com>
2021-04-06 23:21:13 +0700

    [nanda] add service login

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crowfunding/handler"
 	"crowfunding/user"
+	"fmt"
 	"log"
 
 	"github.com/gin-gonic/gin"
@@ -12,7 +13,7 @@ import (
 
 func main() {
 	dsn := "root:@tcp(127.0.0.1:3306)/dbcrowfunding?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})	
 
 	if err != nil{ 
 		log.Fatal(err.Error())
@@ -20,6 +21,22 @@ func main() {
 
 	userRepository := user.NewRepository(db)
 	userService := user.NewService(userRepository)
+
+	input := user.LoginUserInput{
+		Email: "testsa1@gmail.com",
+		Password: "qwerty123s",
+	}
+	user, err := userService.Login(input)
+
+	if err != nil {
+		fmt.Println("terjadi kesalahan")
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println(user.Email)
+	fmt.Println(user.Name)
+	
 	userHandler := handler.NewUserHandler(userService)
 
 	router := gin.Default()
--- user/service.go
+++ user/service.go
@@ -1,9 +1,16 @@
 package user
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"errors"
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
 
 type Service interface {
 	RegisterUser(input RegisterUserInput) (User, error)
+	Login(input LoginUserInput) (User, error)
 }
 
 type service struct {
@@ -34,4 +41,26 @@ func (s *service) RegisterUser(input RegisterUserInput) (User, error) {
 	}
 
 	return newUser, nil
+}
+
+func (s *service) Login(input LoginUserInput) (User, error) {
+	email := input.Email
+	password := input.Password
+
+	user, err := s.repository.FindByEmail(email)
+	if err != nil {
+		return user , err
+	}
+
+	if user.ID == 0 {
+		return user, errors.New("email or password wrong!")
+	}
+
+	fmt.Println(user.Password)
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if err != nil {
+		return user, errors.New("email or password wrong!")
+	}
+
+	return user, nil
 }
