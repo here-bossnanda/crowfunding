f086ee1993b1a3c19c4170104e6d0d4b4d4b5ca7
here-bossnanda <m.trinandanoviardy@gmail.com>
2021-04-07 01:45:49 +0700

    [nanda] add service auth validate token

--- auth/service.go
+++ auth/service.go
@@ -1,9 +1,15 @@
 package auth
 
-import "github.com/dgrijalva/jwt-go"
+import (
+	"errors"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
 
 type Service interface {
 	GenerateToken(userId int) (string, error)
+	ValidateToken(token string) (*jwt.Token, error)
 }
 
 type jwtService struct {
@@ -25,4 +31,20 @@ func (s *jwtService) GenerateToken(userId int) (string, error) {
 		return signedToken, err
 	}
 	return signedToken, nil
+}
+
+func (s *jwtService) ValidateToken(token string) (*jwt.Token, error) {
+	decodedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error){
+		_,ok := token.Method.(*jwt.SigningMethodHMAC) 
+		if !ok {
+			return nil, errors.New("invalid token")
+		}
+		return []byte(SECRET_KEY), nil
+	})
+
+	if err != nil {
+		return decodedToken, err
+	}
+
+	return decodedToken, nil
 }
